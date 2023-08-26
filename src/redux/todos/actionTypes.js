9dd1ad7eaa60d9b2b5c89176795feb458ec0bfbc
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-08-26 21:23:48 +0600

    done with commit

---
+++ src/redux/todos/actionTypes.js
@@ -0,0 +1,6 @@
+export const  ADDED = 'todos/added';
+export const  ToGGLED = 'todos/toggled';
+export const COLORSELECTED = 'todos/colorSelected';
+export const DELETED = "todos/deleted";
+export const ALLCOMPLETED = "todos/allCompleted";
+export const CLEARCOMPLETED = "todos/clearCompleted"
