960da4084ea307f20204726adca2945e4e4a5c75
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-08-27 21:37:52 +0600

    todo and todolist ui task done

--- src/redux/todos/initialState.js
+++ src/redux/todos/initialState.js
@@ -3,13 +3,15 @@ export  const initialState = [
         id: 1,
         text: "Learn react js",
         completed: true,
+        color: "green"
         
 
     },
     
     {id: 2,
     text: "Learn Redux JS",
-    completed: false
+    completed: false,
+    color: "red"
 
     }
 ]
